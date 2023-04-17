d566b0f58450e8a26b8fe5940bd7bb3e7d4000b1
DaMangledMan <jacobh52003@gmail.com>
2023-04-17 17:23:50 -0600

    Finished Prep 1, Started Prep 2
    Completed Prep 1.
    
    Finished most of Prep 2, all that is left is to fill in the catch phrase.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade_percent = int.Parse("0");
+
+        while (true)
+        {
+            try
+            { 
+                Console.Write("What is the percentage score for your class ( 0 - 100 )? ");
+                grade_percent = int.Parse(Console.ReadLine());
+
+                if (grade_percent >= 0 && grade_percent <= 100)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your Input was not in the expected range.");
+                }
+            }
+            catch ()
+            {
+                Console.WriteLine("the input was not in the correct format. Integers only.");
+            }
+        }
+
+        string grade_letter = "o";
+
+        if (grade_percent >= 90)
+        {
+            grade_letter = "A";
+        }   else if (grade_percent >= 80)
+        {
+            grade_letter = "B";
+        }   else if (grade_percent >= 70)
+        {
+            grade_letter = "C";
+        }   else
+        {
+            grade_letter = "F";
+        }
+        
+        Console.WriteLine($"Your letter grade is {grade_letter}");
+
+        if (grade_letter == "F")
+        {
+            Console.WriteLine("Better luck next time.");
+        }   else
+        {
+            Console.WriteLine("Congratulations on passing.");
+        }
     }
 }
