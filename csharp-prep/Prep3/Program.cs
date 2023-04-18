b74abe95bfe02c634d3345bc08bee0aeef3f1706
DaMangledMan <jacobh52003@gmail.com>
2023-04-18 14:03:12 -0600

    1st Draft of Prep 3
    Prep 3 seems to be working. I just have to test it.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magic_number;
+        int guess_number;
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("What is the magic number? ");
+                magic_number = int.Parse(Console.ReadLine());
+
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("the input was not in the correct format. Integers only.");
+            }
+        }
+        
+        while (true)
+        {
+            try
+            {
+                Console.Write("What is your guess? ");
+                guess_number = int.Parse(Console.ReadLine());
+
+            } catch (FormatException)
+            {
+                Console.WriteLine("the input was not in the correct format. Integers only.");
+                continue;
+            }
+            
+            if (guess_number > magic_number)
+            {
+                Console.WriteLine("Lower!");
+            } else if (guess_number < magic_number)
+            {
+                Console.WriteLine("Higher!");
+            } else
+            {
+                Console.WriteLine("Correct!");
+                break;
+            }
+        }
     }
 }
