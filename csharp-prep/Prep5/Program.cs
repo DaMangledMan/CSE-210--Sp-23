db0236ff8736d7c3fad14babb23c40c153e508e4
DaMangledMan <jacobh52003@gmail.com>
2023-04-19 11:43:31 -0600

    Finalized Prep 5
    Completed all requirements for Prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static int square_number(int input)
+        {
+            return input * input;
+        }
+
+        string name;
+        int number;
+        
+        
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();
+
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Please enter your favorite number: ");
+                number = int.Parse(Console.ReadLine());
+
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("the input was not in the correct format. Integers only.");
+            }
+        }
+        
+        Console.WriteLine($"{name}, the square of your number is {square_number(number)}");
     }
 }
