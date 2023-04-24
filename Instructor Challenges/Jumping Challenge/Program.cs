790bde521582ccc7549fa1c4244b96096d02df59
DaMangledMan <jacobh52003@gmail.com>
2023-04-24 14:14:13 -0600

    Started Instructor Challenge
    Started Instructor Challenge for number jumping through a list.

---
+++ Instructor Challenges/Jumping Challenge/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list_of_numbers = new List<int> {1, 1, 1};
+        
+        int current_index = 0;
+        int final_index = list_of_numbers.Count - 1;
+
+        while (current_index != final_index)
+        {
+            
+            (list_of_numbers[current_index]);
+            break;
+        }
+        Console.Writeline();
+    }
+}
