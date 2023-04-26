72b93539e1e0b085c89bc0118ef16741698fd394
DaMangledMan <jacobh52003@gmail.com>
2023-04-26 10:12:37 -0600

    started working on Instructor challenge
    began working on Instructor Challenge

--- Instructor Challenges/Jumping Challenge/Program.cs
+++ Instructor Challenges/Jumping Challenge/Program.cs
@@ -4,17 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> list_of_numbers = new List<int> {1, 1, 1};
-        
+        List<int> list_of_numbers = new List<int> {5, 1, 1};
+        List<int> useless_indexes = new List<int> ();
+
         int current_index = 0;
         int final_index = list_of_numbers.Count - 1;
 
+        useless_indexes.Add(0);
+        
+
         while (current_index != final_index)
         {
+            if (((current_index - list_of_numbers[current_index]) == final_index) || ((current_index + list_of_numbers[current_index]) == final_index))
+            {
+                Console.WriteLine("Yes, there is a way to jump through the list.");
+                break;
+            }
+            if (((final_index - list_of_numbers[current_index]) < 0) || ((current_index - list_of_numbers[current_index]) <= 0))
+            {
+                useless_indexes.Add(current_index);
+                continue;
+            } else
+            {
+                foreach (int n in useless_indexes)
+                {
+                    if ((current_index - list_of_numbers[current_index]) == n)
+                    {
+                        foreach (int m in useless_indexes)
+                        {
+                            if ((current_index + list_of_numbers[current_index]) == m)
+                            {
+                                useless_indexes.Add(current_index);
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
             
-            (list_of_numbers[current_index]);
+            Console.WriteLine(list_of_numbers[current_index]);
             break;
         }
-        Console.Writeline();
     }
 }
