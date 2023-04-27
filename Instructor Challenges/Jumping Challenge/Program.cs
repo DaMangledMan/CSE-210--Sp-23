d135eecca8f0466b15e1232adf29a954fc4b148c
DaMangledMan <jacobh52003@gmail.com>
2023-04-27 14:13:40 -0600

    finished the instructor challenge for the jumping list

--- Instructor Challenges/Jumping Challenge/Program.cs
+++ Instructor Challenges/Jumping Challenge/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random Rnd = new Random();
+                
         // checks if the move is possible and returns true if it is
         static bool possible_move(string direction, int current_index, int final_index, int distance)
         {
@@ -28,20 +30,6 @@ class Program
                 }
             }
             return possible;
-        }
-
-        // checks if the move is possible and returns false if it is
-        static bool not_possible_move(string direction, int current_index, int final_index, int distance)
-        {
-            bool true_false = new bool();
-            if (possible_move(direction, current_index, final_index, distance))
-            {
-                true_false = false;
-            } else
-            {
-                true_false = true;
-            }
-            return true_false;
         }
 
         // checks if the move is useful and returns true if it is
@@ -74,69 +62,74 @@ class Program
 
             return true;
         }
-        
-        // returns the new index after adding/subtracting the distance from the current index
-        static int make_move(string direction, int current_index, int distance)
+
+        // creates the lists used and needed for the program
+        List<int> puzzle_list = new List<int> {};
+        List<int> current_line = new List<int> {0};
+        List<int> useless_indexes = new List<int> {0};
+
+        // fills the puzzle list with a random number of random numbers
+        for (int i = 0; i < Rnd.Next(30, 151); i++)
         {
-            int new_index = new int();
-
-            if (direction == "R")
-            {
-                new_index = current_index + distance;
-            } else if (direction == "L")
-            {
-                new_index = current_index - distance;
-            }
-
-            return new_index;
+            puzzle_list.Add(Rnd.Next(1, 21));
         }
-        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        List<int> puzzle_list = new List<int> {5, 1, 1};
-        List<int> current_line = new List<int> ();
-        List<int> useless_indexes = new List<int> ();
-
-        int current_index = 0;
+        // sets what the index of the final position of the puzzle list
         int final_index = puzzle_list.Count - 1;
 
-        useless_indexes.Add(0);
-        
-        int distance = puzzle_list[current_index];
-
-        
-
-        // this block checks if any move is useful
-        // it makes the move if it is or adds it to the useless index list otherwise
-        if (useful_move("R", current_index, distance, current_line, useless_indexes))
+        // displays the puzzle list for user to see
+        Console.WriteLine("\n\nThe puzzle list is:");
+        foreach (int i in puzzle_list)
         {
-            current_index = make_move("R", current_index, distance);
-        } else if (useful_move("L", current_index, distance, current_line, useless_indexes))
-        {
-            current_index = make_move("L", current_index, distance);
-        } else
-        {
-            useless_indexes.Add(current_index);
+            Console.Write($"{i}, ");
         }
+        Console.WriteLine("\n\n");
 
+        // starts loop
+        while (true)
+        {
+            // sets the new current index
+            int current_index = current_line[current_line.Count() - 1];
 
+            // checks if we are already at the end
+            if (current_index == final_index)
+            {
+                Console.WriteLine("There is a way to the end. The first line of indexes we found was:");
+                foreach (int i in current_line)
+                {
+                    Console.Write($"{i}  ");
+                }
+                Console.WriteLine("\n\n");
+                break;
+            }
+            
+            //sets the distance we are able to move
+            int distance = puzzle_list[current_index];
 
-        
-
-
-
-
+            // checks possibility and usefulness of moving to the right
+            if (possible_move("R", current_index, final_index, distance) && useful_move("R", current_index, distance, current_line, useless_indexes))
+            {
+                // adds the new index to the current line
+                current_line.Add(current_index + distance);
+            } 
+            // checks possibility and usefulness of moving to the left
+            else if (possible_move("L", current_index, final_index, distance) && useful_move("L", current_index, distance, current_line, useless_indexes))
+            {
+                // adds the new index to the current line
+                current_line.Add(current_index - distance);
+            }
+            // checks if the current index is 0
+            else if (current_index == 0)
+            {
+                Console.WriteLine("There is no possible route to the final index.\n\n");
+                break;
+            }            
+            // if no move is possible or useful it marks the current index as useless
+            else
+            {
+                useless_indexes.Add(current_index);
+                current_line.RemoveAt(current_line.Count() - 1);
+            }
+        }
     }
 }
