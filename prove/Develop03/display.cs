7e2c4a7d600b5f0170aac36a420441ff58c3685b
DaMangledMan <jacobh52003@gmail.com>
2023-05-30 14:04:35 -0600

    changed the scripture

--- prove/Develop03/display.cs
+++ prove/Develop03/display.cs
@@ -1,7 +1,7 @@
 public class Display
 {
     // calls child classes
-    Scripture scripture = new Scripture("1 Nephi 2:15", "And my father dwelt in a tent.");
+    Scripture scripture = new Scripture("1 Nephi 1:1", "I, Nephi, having been born of goodly parents, therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless, having been highly favored of the Lord in all my days; yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days.");
     Hide hide;
 
     public Display(){
