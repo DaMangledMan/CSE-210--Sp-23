11ace2b7d997e3d7552d625e46b380166c58d656
DaMangledMan <jacobh52003@gmail.com>
2023-06-08 20:21:29 -0600

    initiated and setup all the files for the mindfulness program

---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,28 @@
+public class menu
+{
+    // Attributes
+
+    // Constructor
+
+    // Methods
+
+    public void Menu()
+    {
+
+    }
+
+    public void breathing()
+    {
+
+    }
+
+    public void reflection()
+    {
+        
+    }
+
+    public void listing()
+    {
+        
+    }
+}
